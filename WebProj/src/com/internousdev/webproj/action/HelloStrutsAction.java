719e7d8fe4f9767073b0cad79129028d5e3e2ce7
kanav0208 <kanav0208@yahoo.co.jp>
2018-01-19 16:56:11 +0900

    1/19

---
+++ WebProj/src/com/internousdev/webproj/action/HelloStrutsAction.java
@@ -0,0 +1,14 @@
+package com.internousdev.webproj.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloStrutsAction extends ActionSupport {
+	public class HelloStrutsAction extends ActionSupportt {
+		public String execute() {
+			return SUCCESS;
+			
+		}
+	}
+	
+
+}
---
+++ WebProj3/src/com/internous/webproj3/util/DBConnector.java
@@ -0,0 +1,5 @@
+package com.internous.webproj3.util;
+import java.sql.Connector
+public class DBConnector {
+
+}
